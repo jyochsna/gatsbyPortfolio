d863a0d5a0bdbc2e89191319efff41405af7024a
jyochsna <55848802+jyochsna@users.noreply.github.com>
2020-06-06 19:08:11 -0700

    made a second test change

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
 
 export default function Home() {
-  return <div>This is my Portfolio created in gatsby</div>
+  return (
+    <div>
+      <h1 style={{color:"orange"}}>This is my Portfolio created in gatsby</h1>
+      <h2>The site is under constrution.</h2>
+    </div>
+  )
 }
